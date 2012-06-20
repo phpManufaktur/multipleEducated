6d62838c6ced21f1b5a48789bb1f1d582290fff8
hertsch <ralf.hertsch@phpmanufaktur.de>
2012-06-20 04:14:28 +0200

    Release 0.16
    changed WebsiteBaker version check, added CHANGELOG, removed info.txt,
    changed license from GNU GPL to MIT license, added LICENSE, inserted
    check if file is saved in UTF-8 format to the language files, updated
    all header informations, added README.md

--- info.php
+++ info.php
@@ -2,45 +2,43 @@
 
 /**
  * multipleEducated
- * 
- * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ *
+ * @author Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
  * @link http://phpmanufaktur.de
- * @copyright 2011
- * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
- * @version $Id$
- * 
- * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ * @copyright 2009 - 2012
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
  */
 
-// try to include LEPTON class.secure.php to protect this file and the whole CMS!
-if (defined('WB_PATH')) {	
-	if (defined('LEPTON_VERSION')) include(WB_PATH.'/framework/class.secure.php');
-} elseif (file_exists($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php')) {
-	include($_SERVER['DOCUMENT_ROOT'].'/framework/class.secure.php'); 
-} else {
-	$subs = explode('/', dirname($_SERVER['SCRIPT_NAME']));	$dir = $_SERVER['DOCUMENT_ROOT'];
-	$inc = false;
-	foreach ($subs as $sub) {
-		if (empty($sub)) continue; $dir .= '/'.$sub;
-		if (file_exists($dir.'/framework/class.secure.php')) { 
-			include($dir.'/framework/class.secure.php'); $inc = true;	break; 
-		} 
-	}
-	if (!$inc) trigger_error(sprintf("[ <b>%s</b> ] Can't include LEPTON class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+// include class.secure.php to protect this file and the whole CMS!
+if (defined('WB_PATH')) {
+  if (defined('LEPTON_VERSION'))
+    include(WB_PATH.'/framework/class.secure.php');
 }
-// end include LEPTON class.secure.php
+else {
+  $oneback = "../";
+  $root = $oneback;
+  $level = 1;
+  while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+    $root .= $oneback;
+    $level += 1;
+  }
+  if (file_exists($root.'/framework/class.secure.php')) {
+    include($root.'/framework/class.secure.php');
+  }
+  else {
+    trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+  }
+}
+// end include class.secure.php
 
 $module_directory     = 'educated';
 $module_name          = 'multipleEducated';
 $module_function      = 'tool';
-$module_version       = '0.15';
+$module_version       = '0.16';
 $module_status        = 'Stable';
 $module_platform      = '2.8.x';
 $module_author        = 'Ralf Hertsch, Berlin (Germany)';
-$module_license       = 'GNU GPL';
+$module_license       = 'MIT License (MIT)';
 $module_description   = '"Bildungshappen" for Website Baker';
 $module_home          = 'http://phpmanufaktur.de';
 $module_guid          = 'E579E333-829B-412F-89C0-9A852EF75ABC';
-
-
-?>
